16aae890f259233a44f664d74be3b32acaac417c
DanaAlomari <danaalomari27@gmail.com>
2022-11-02 01:37:15 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Form from './Form';
+import Nav from './Nav';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+
+  
+  <React.StrictMode>
+    <Nav/>
+<Form/>
+
+  </React.StrictMode>
+  
+);
+
+reportWebVitals();
